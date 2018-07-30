9ec14fbaa9c3df170bdfe7a160e453d2752b767b
Aaron Fulmer <33696641+AaronFulmer@users.noreply.github.com>
2018-07-30 12:11:14 -0400

    First iteration of sensor polling file
    combined the BME280 and TSL2561 example libraries into one.

---
+++ Biomonitoring_Arduino_Sketch.ino
@@ -0,0 +1,125 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include <Adafruit_TSL2561_U.h>
+
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11
+#define BME_CS 10
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK);                            // creating an object for the BME280 sensor with software SPI
+
+Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);  // creating an object for the TSL2561 sensor with id of 12345
+
+unsigned long delayTime;
+
+/*
+  This Method is used to configure the TSL2561 Sensor.
+  (Change values in this method to affect the sensor)
+*/
+void configureSensor(void)
+{
+  /* You can also manually set the gain or enable auto-gain support */
+  // tsl.setGain(TSL2561_GAIN_1X);      /* No gain ... use in bright light to avoid sensor saturation */
+  // tsl.setGain(TSL2561_GAIN_16X);     /* 16x gain ... use in low light to boost sensitivity */
+  tsl.enableAutoRange(true);            /* Auto-gain ... switches automatically between 1x and 16x */
+  
+  /* Changing the integration time gives you better sensor resolution (402ms = 16-bit data) */
+  tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS);      /* fast but low resolution */
+  // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_101MS);  /* medium resolution and speed   */
+  // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_402MS);  /* 16-bit data but slowest conversions */
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+  // -------------------------------------------------------------
+  // Setting up the BME280 Sensor
+  // -------------------------------------------------------------
+  
+  bool status;
+    
+    // default settings
+    // (you can also pass in a Wire library object like &Wire2)
+    status = bme.begin();  
+    if (!status) {
+        Serial.println("Could not find a valid BME280 sensor, check wiring!");
+        while (1);
+    }
+    
+    Serial.println("BME280 Successfully Configured");
+    delayTime = 300000;
+
+    Serial.println();
+    
+    // ----------------------------------------------------------
+    // Setting up the TSL2561 Sensor
+    // ----------------------------------------------------------
+
+    if(!tsl.begin())
+  {
+    /* There was a problem detecting the TSL2561 ... check your connections */
+    Serial.print("Ooops, no TSL2561 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+
+   configureSensor();
+   Serial.println("TSL2561 Successfully Configured");
+   
+}
+
+/*
+    This method is used to print the data from the BME280 Sensor
+    (Called from the loop() function)
+*/
+void printValues() {
+    Serial.print("Temperature = ");
+    Serial.print(bme.readTemperature());
+    Serial.println(" *C");
+
+    Serial.print("Pressure = ");
+
+    Serial.print(bme.readPressure() / 100.0F);
+    Serial.println(" hPa");
+
+    Serial.print("Approx. Altitude = ");
+    Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+    Serial.println(" m");
+
+    Serial.print("Humidity = ");
+    Serial.print(bme.readHumidity());
+    Serial.println(" %");
+
+    Serial.println();
+
+}
+/*
+    This is where the magic happens. First, the Arduino requests and displays the data from the TSL2561(Lux) Sensor.
+    Next, it requests and displays the data from the BME280(Environment) Sensor.
+    Then, it waits for 5 minutes and does it again.
+*/
+void loop() {
+  
+  /* Get a new sensor event */ 
+  sensors_event_t event;
+  tsl.getEvent(&event);
+ 
+  /* Display the results (light is measured in lux) */
+  if (event.light)
+  {
+    Serial.print(event.light); Serial.println(" lux");
+  }
+  else
+  {
+    /* If event.light = 0 lux the sensor is probably saturated
+       and no reliable data could be generated! */
+    Serial.println("Sensor overload");
+  }
+  printValues();
+  delay(delayTime);
+}
