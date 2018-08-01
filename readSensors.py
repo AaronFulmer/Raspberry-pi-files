413bb1efb436defb502354c26e99aad5ce00182c
Aaron Fulmer <33696641+AaronFulmer@users.noreply.github.com>
2018-08-01 11:22:00 -0400

    Update: Fixed so it works
    - Added a loop to read the buffer for more consistency and the potential for more or less data to be transferred.
    - Fixed various lines to work with python (was working off the top of my head through other languages before testing).

--- readSensors.py
+++ readSensors.py
@@ -4,37 +4,28 @@
 	read the sensors wired to the arduino and log the data in a
 	text file. 
 
-	I have not tested it with the RPi and Arduino yet.
-
 '''#################################################################
 import serial
 import time
 
-port = "/dev/ttyACM0"  # Serial port for computer this program runs on (this one is what the port was on my Raspberry Pi)
+port = "COM3"  # Serial port for computer this program runs on (this one is what the port was on my PC)
 try:
 	serial_connection = serial.Serial(port, baudrate=9600, timeout=1)  # Establishing a serial connection at 9600 baudrate
 	print "Connected!"                                                 # Confirmation
 except serial.SerialException:                                         # Catching an invalid port error
 	print "Connection Failure!"
 
-serial_connection.close()       # Close the serial connection
-time.sleep(0.5)                 # Delay half of a second
-serial_connection.open()        # Open the serial connection
-time.sleep(0.5)                 # Delay half of a second
+time.sleep(2)                   # Delay 2 seconds to allow for initialisation time before sending any commands.
 file = open("output.txt", "a")  # Open a text file (mine is called output.txt) in the program's source folder in append mode ("a")
+serial_connection.write(b'r')   # Sending the command to the arduino to read the sensors and return the data
 
-serial_connection.write("read sensors")   # Sending the command to the arduino to read the sensors and return the data
-
-time.sleep(1)
+time.sleep(2)                   # Delay 2 seconds to allow the Arduino to receive the data, make necessary calculations, and
+                                # 	send the data over the serial port
 
 '''
-There are 5 outputs from the arduino with sensor data (Lux, temperature, 
-estimated altitude, barometric pressure, and humidity) so for now it is
-just reading the serial port 5 times. This will be changed later but it
-works for now
+The following loop checks that there is something left in the buffer to read.
+If there is, it loops over reading each line and writing it to the text file.
 '''
-file.print(serial_connection.readLine())
-file.print(serial_connection.readLine())
-file.print(serial_connection.readLine())
-file.print(serial_connection.readLine())
-file.print(serial_connection.readLine())
+while serial_connection.inWaiting():
+	file.write(serial_connection.readline())
+file.close()
